868f02b691f28bd88c0a2c44183a8c6dabb5f87f
abayomi27 <abayomi.s.yusuf@gmail.com>
2016-01-03 16:03:10 +0000

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :posts
   get 'signup', to: 'users#new'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160101220841) do
+ActiveRecord::Schema.define(version: 20160103144222) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "post"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
